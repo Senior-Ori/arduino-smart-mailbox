8f71ccf0df3d3f115e3a7b0412477236c5a79766
senior_Ori <ori8834@gmail.com>
2022-11-28 18:10:39 +0200

    trying to make faster serial communication

--- bitch_nigga.ino
+++ bitch_nigga.ino
@@ -1,22 +1,34 @@
 
 //int analogPin = 3;
 //int data = 0;
+int i= 0;
+int g=0;
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 
+//receiving time of the data takes unnecessary amount of time..
 void loop() {
   if (Serial.available() > 0) {
     String dataInput = Serial.readStringUntil('\r');
-   
+
     if (dataInput == "on") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.write("LED Is On");
+      PORTB = B00100000;
+      String lol = String("LED Is On, took:[")+ String(i)+String(",")+String(g)+String(" x IntMaxNubmer(2,147,483,647)")+String("] attempts.");
+      Serial.write(lol.c_str());
     }
-    if (dataInput=="off") {
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.write("LED Is Off");
+    if (dataInput == "off") {
+      PORTB = B00000000;
+      String lol = String("LED Is On, took:[")+ String(i)+String(",")+String(g)+String(" x IntMaxNumber(2,147,483,647)")+String("] attempts.");
+      Serial.write(lol.c_str());
     }
+  }else if (i<0) {
+  i=0;
+  g++;
+  }
+  else {
+    i++;
   }
 }
