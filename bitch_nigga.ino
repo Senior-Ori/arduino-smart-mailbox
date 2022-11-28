c8fa5d6d66e45f1c8940690a0ebfbc2a42899e3a
senior_Ori <ori8834@gmail.com>
2022-11-28 18:20:48 +0200

    Update bitch_nigga.ino
    delete & optimize

--- bitch_nigga.ino
+++ bitch_nigga.ino
@@ -1,34 +1,35 @@
 
-//int analogPin = 3;
-//int data = 0;
-int i= 0;
-int g=0;
+const unsigned int MAX_INPUT = 50;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  
+}
+
+void processData (const char * data){
+
+}
+
+void processIncomingByte(const byte inByte){
+  static char inputLine [MAX_INPUT];
+  static unsigned int inputPos = 0;
 }
 
 //receiving time of the data takes unnecessary amount of time..
 void loop() {
   if (Serial.available() > 0) {
+    //this line take 1 sec, i shall do it manualy
     String dataInput = Serial.readStringUntil('\r');
 
     if (dataInput == "on") {
       PORTB = B00100000;
-      String lol = String("LED Is On, took:[")+ String(i)+String(",")+String(g)+String(" x IntMaxNubmer(2,147,483,647)")+String("] attempts.");
+      String lol = String("LED Is On, took:[")+String(" x IntMaxNubmer(2,147,483,647)")+String("] attempts.");
       Serial.write(lol.c_str());
     }
     if (dataInput == "off") {
       PORTB = B00000000;
-      String lol = String("LED Is On, took:[")+ String(i)+String(",")+String(g)+String(" x IntMaxNumber(2,147,483,647)")+String("] attempts.");
+      String lol = String("LED Is On, took:[")+String(" x IntMaxNumber(2,147,483,647)")+String("] attempts.");
       Serial.write(lol.c_str());
     }
-  }else if (i<0) {
-  i=0;
-  g++;
-  }
-  else {
-    i++;
   }
 }
