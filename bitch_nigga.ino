5c85b57e6cdfff132d90b9c57fdad025fbd210cb
senior_Ori <ori8834@gmail.com>
2022-11-30 01:08:26 +0200

    Update bitch_nigga.ino

--- bitch_nigga.ino
+++ bitch_nigga.ino
@@ -2,39 +2,55 @@
 #include <ArduinoJson.hpp>
 
 const unsigned int MAX_INPUT = 256;
+byte ledMapB=0b00000000;
 
 void setup() {
   Serial.begin(57600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  DDRD &= B11111111;
 }
 
 void processData(const char* input) {
-  StaticJsonDocument<256> doc;//set efficient size for jsonDoc
-  DeserializationError err=deserializeJson(doc,input);
+  StaticJsonDocument<256> doc;  //set efficient size for jsonDoc
+  DeserializationError err = deserializeJson(doc, input);
 
   //test --> {"command":"-1","leds":[1,0,0,0]}
+  // {"command":"triggerLed","leds":[-1,-1,-1,-1]}
 
   if (err) {
-  Serial.print("ERRROR: ");
-  Serial.println(err.c_str());
-  return; //break, cos you got error..
+    Serial.print("ERRROR: ");
+    Serial.println(err.c_str());
+    return;  //break, cos you got error..
   }
 
   const char* command = doc["command"];
   int ledOnOff[4];
-  for (int i=0;i<4;i++) ledOnOff[i]=doc["leds"][i];
-
-  
+  for (int i = 0; i < 4; i++) ledOnOff[i] = doc["leds"][i];
 
 
 
-  if (ledOnOff[0]) {
-    PORTB = B00100000; Serial.write("Led Is On");}
-    else{PORTB = B00000000; Serial.write("Led Is Off");}
 
-  if (command == "triggerLed") {
-    PORTB ^= -1;
-    Serial.write("LED has been triggered");
+
+  if (ledOnOff[0] == 1) {
+    ledMapB = 0b00100000;
+    PORTB = ledMapB;
+    Serial.write("Led Is On");
+  }
+  if (ledOnOff[0] == 0) {
+    ledMapB=0b00000000;
+    PORTB = ledMapB;
+    Serial.write("Led Is Off");
+  }
+
+  if (String(command) == "triggerLed") {
+    //i think there is a interruption because of the interruption bit
+    ledMapB ^= -1;
+    PORTB = ledMapB;
+    //Serial.write("LED has been triggered");
+    Serial.print("PORTB: ");
+    Serial.println(PORTB,BIN);
+    Serial.print("ledMapB: ");
+    Serial.println(ledMapB,BIN);
+    
   }
 }
 
@@ -69,7 +85,6 @@ void processIncomingByte(const byte inByte) {
 void loop() {
   while (Serial.available() > 0)
     processIncomingByte(Serial.read());
-    
 }
 
 
