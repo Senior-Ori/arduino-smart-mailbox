e4931ad53cecd405707a4d4ea394b382947e6f78
senior_Ori <ori8834@gmail.com>
2022-12-01 19:40:11 +0200

    loop in parallel to tkinter mainloop

--- bitch_nigga.py
+++ bitch_nigga.py
@@ -2,95 +2,61 @@ import serial
 from tkinter import *
 import tkinter as tk
 import time
-
 # import json
-ser = serial.Serial('COM5', baudrate=57600, timeout=.01)
-time.sleep(1)
-
-DataDict = {"command": "", "leds": [-1, -1, -1, -1]}
 
 
-# DataDict = {"command": "", "leds": [-1, -1, -1, -1]}
-# DataDict["command"]="triggerLed"
-# print(f"{DataDict}\r\n".encode())
 
-def writeJsonHandler(name, values):
-    DataDict[name] = values
-    # if name=="triggerLed": DataDict["leds"] = [-1,-1,-1,-1]
-    # {"command":"triggerLed","leds":[-1,-1,-1,-1]}
-    ser.write(f"{DataDict}\r\n".encode())
-    DataDict["command"] = ""
-    return
-
-
-def turnOnLED():
-    if DataDict["leds"] == [1, 0, 1, 0]:
-        return
-    writeJsonHandler("leds", [1, 0, 1, 0])
-    while True:
-        #     print(f"Loading... number of checks for serial input:~=[{i}]")
-        if ser.in_waiting:
-            text1 = ser.readline().decode('utf')
-            break
-    my_var.set(str(text1))
-
-
-def turnOffLED():
-    if DataDict["leds"] == [0, 0, 0, 0]:
-        return print(
-            "Leds are already ON, therefore no matter how much you will hammer this button it will not do anything")
-    writeJsonHandler("leds", [0, 0, 0, 0])
-
-    while True:
-        #     print(f"Loading... number of checks for serial input:~=[{i}]")
-        if ser.in_waiting:
-            text1 = ser.readline().decode('utf')
-            break
-    my_var.set(str(text1))
-
-
-def turnOnOffLED():
-    DataDict["leds"] = [-1, -1, -1, -1]
-    writeJsonHandler("command", "triggerLed")
-    while True:
-        #     print(f"Loading... number of checks for serial input:~=[{i}]")
-        if ser.in_waiting:
-            text1 = ser.readline().decode('utf')
-            break
-    my_var.set(str(text1))
-
-
-# creating TK-inter window
-root = Tk()
-root.title('Blink GUI')
+root = Tk() # creating TK-inter window
+root.title('SmartMail-[BOX]')
 root.geometry("350x350")
 root.configure(bg="#141414")
 text1 = "initial value"
 my_var = tk.StringVar()
 my_var.set("First click")
 
+ser = serial.Serial('COM5', baudrate=57600, timeout=.01)  # set to the overflow ceiling of arduino nano, I think..
+time.sleep(1)
+
+DataDict = {"command": "", "leds": [-1, -1, -1, -1]}
+checkFlag = 0
+index = {"times": 1}
+def writeJsonHandler(name, values):
+    DataDict[name] = values
+    ser.write(f"{DataDict}\r\n".encode())  # write to arduino in a json structure format.
+    DataDict["command"] = ""  # reset commands.
+
+def turnOnLED():
+    if DataDict["leds"] == [1, 0, 1, 0]: return
+    writeJsonHandler("leds", [1, 0, 1, 0])
+    checkFlag = 1
+
+def turnOffLED():
+    if DataDict["leds"] == [0, 0, 0, 0]: return
+    writeJsonHandler("leds", [0, 0, 0, 0])
+    checkFlag = 1
+
+def turnOnOffLED():
+    DataDict["leds"] = [-1, -1, -1, -1]
+    writeJsonHandler("command", "triggerLed")
+    checkFlag = 1
+
+
+
+def update():
+    index["times"]+=1
+    label1.config(text=index["times"])
+    label1.after(1,update)
+
+
+#if ser.in_waiting:
+    # text1 = ser.readline().decode('utf')
+    # break
+
 tk.Button(root, name="btn_On", text="on", width=10, command=turnOnLED).grid(row=0, column=0)
 tk.Button(root, name="btn_Off",text="off", width=10, command=turnOffLED).grid(row=0, column=1)
 tk.Button(root, name="btn_Trig",text="Trigger", width=10, command=turnOnOffLED).grid(row=0, column=2)
+label1 = tk.Label(root, name="btn_loop",text="1", width=10)
+label1.grid(row=0, column=3)
 tk.Label(root,name="prompt_label", textvariable=my_var, width=30, fg="red").grid(row=1, column=0, columnspan=3,rowspan=2)
-
+label1.after(1000,update)
 root.mainloop()
-
-## firstCode
-# numPoints = 5
-# dataList = [0] * numPoints
-#
-#
-# def getValues():
-#     ser.write(b'g')
-#     arduinoData = ser.readline().decode('ascii')
-#     return arduinoData
-#
-#
-# while True:
-#     userInput = input('Get data point?')
-#     if userInput == 'y':
-#         for i in range(0,len(dataList),1):
-#             data = getValues()
-#             dataList[i]=data
-#         print(dataList)
