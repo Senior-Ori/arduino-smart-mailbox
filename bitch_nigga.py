6f724c88e71fbb6417288f2fcef3d323c3589cd1
senior_Ori <ori8834@gmail.com>
2022-12-05 23:28:47 +0200

    All work!, ugly syntax though...

--- bitch_nigga.py
+++ bitch_nigga.py
@@ -14,10 +14,10 @@ root.geometry("350x350")
 root.configure(bg="#141414")
 text1 = "initial value"
 my_var = tk.StringVar()
+mail0_var = tk.StringVar()
 mail1_var = tk.StringVar()
 mail2_var = tk.StringVar()
 mail3_var = tk.StringVar()
-mail4_var = tk.StringVar()
 
 my_var.set("First click")
 
@@ -25,29 +25,19 @@ ser = serial.Serial('COM5', baudrate=57600, timeout=.01)  # set to the overflow 
 time.sleep(1)
 
 DataDict = {"command": "", "leds": [-1, -1, -1, -1]}
-DataInput = {"ir":[],"msg":""}
+DataInput = {}
 Flag = {"leds":False}
 index = {"times": 1}
 def getJson():
-    data = f"{ser.readline().decode('utf')}"
-    indexMs = data.find(f"\"msg\"")
-    if indexMs:
-        indexLs = data.find("\"}", indexMs)
-        text = str(data[indexMs + 7:indexLs])
-        DataInput["msg"] = text
-        my_var.set(DataInput["msg"])
-    indexMs = data.find(f"\"ir\"")
-    if indexMs:
-        indexLs = data.find(",\"", indexMs)
-        IRs = data[indexMs + 5:indexLs]
-        if DataInput["ir"]!=IRs:
-            DataInput["ir"] = IRs
-            mail1_var.set(DataInput["ir"][0])
-            mail2_var.set(DataInput["ir"][1])
-            mail3_var.set(DataInput["ir"][2])
-            mail4_var.set(DataInput["ir"][3])
-        # my_var.set(DataInput["ir"])
-        # my_var.set(data)
+    DataInput = json.loads(f"{ser.readline().decode('utf')}")
+    my_var.set(DataInput["msg"])
+    if DataInput["ir"]!=[mail0_var,mail1_var,mail2_var,mail3_var]:
+        for n in range(len(DataInput["ir"])):
+            globals()[f'mail{n}_var'].set(DataInput["ir"][n])
+            if DataInput["ir"][n]:
+                globals()[f'mail{n}'].config(bg="green")
+            else:
+                globals()[f'mail{n}'].config(bg="grey")
     return
 
 def clock():
@@ -96,14 +86,14 @@ tk.Button(root, name="btn_Off",text="off", width=10, command=turnOffLED).grid(ro
 tk.Button(root, name="btn_Trig",text="Trigger", width=10, command=turnOnOffLED).grid(row=0, column=2)
 label1 = tk.Label(root, name="btn_loop",text="1", width=10,fg="green",bg="black")
 label1.grid(row=0, column=3)
-mail1 = tk.Label(root, name="mail1",textvariable=mail1_var, width=10,fg="green",bg="black")
-mail1.grid(row=3, column=0)
-mail2 = tk.Label(root, name="mail2",textvariable=mail2_var, width=10,fg="green",bg="black")
-mail2.grid(row=3, column=1)
-mail3 = tk.Label(root, name="mail3",textvariable=mail3_var, width=10,fg="green",bg="black")
-mail3.grid(row=3, column=2)
-mail4 = tk.Label(root, name="mail4",textvariable=mail4_var, width=10,fg="green",bg="black")
-mail4.grid(row=3, column=3)
+mail0 = tk.Label(root, name="mail1",textvariable=mail0_var, width=10,fg="black",bg="black")
+mail0.grid(row=3, column=0)
+mail1 = tk.Label(root, name="mail2",textvariable=mail1_var, width=10,fg="black",bg="black")
+mail1.grid(row=3, column=1)
+mail2 = tk.Label(root, name="mail3",textvariable=mail2_var, width=10,fg="black",bg="black")
+mail2.grid(row=3, column=2)
+mail3 = tk.Label(root, name="mail4",textvariable=mail3_var, width=10,fg="black",bg="black")
+mail3.grid(row=3, column=3)
 tk.Label(root,name="prompt_label", textvariable=my_var, width=50, fg="red").grid(row=1, column=0, columnspan=4,rowspan=2)
 #label1.after(1000,update)
 
